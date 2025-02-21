9773a03fc121350405cd1d4d8afa812de8660523
Nghiêm Văn Quang <nghiemquang2002@gmail.com>
2025-02-21 17:19:49 +0700

    update models: InputEmbedding, Posencode, MultiheadATT

---
+++ models/InputEmbeddings.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from math import sqrt
+
+class InputEmbeddings(nn.Module):
+    def __init__
+    
+    def forward(self
+        
---
+++ models/MultiHeadAttention.py
@@ -0,0 +1,7 @@
+import torch
+import torch.nn as nn
+import math
+
+class MultiHeadAttentionBlock(nn.Module):
+    self.__init__(self, embed_size, heads, dropout=0.0):
+    
